7621260b1901e8947bf4e566a027a9ca8a89e393
Florian Lovasi <lovasifloriian@outlook.hu>
2026-05-28 19:47:27 +0200

    (feat): add bus routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./component/navbar/Navbar";
 import Task from "./page/task/Task";
-import {APP_ROUTES} from "./config/routes.ts";
+import Buses from "./page/buses/Buses";
+import { APP_ROUTES } from "./config/routes.ts";
+import BusDetails from "./page/buses/BusDetails";
+import EditBusPage from "./page/buses/BusEdit";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Navbar />
+  return (
+    <BrowserRouter>
+      <Navbar />
 
-            <Routes>
-                <Route path={APP_ROUTES.TASK} element={<Task />} />
-                <Route path={APP_ROUTES.BUS_CRUD} element={<div style={{color: 'white'}}>Bus CRUD oldal</div>} />
-            </Routes>
-        </BrowserRouter>
-    );
+      <Routes>
+        <Route path={APP_ROUTES.TASK} element={<Task />} />
+
+        <Route path={APP_ROUTES.BUS_CRUD}>
+          <Route index element={<Buses />} />
+          <Route path={APP_ROUTES.BUS_DETAILS} element={<BusDetails />} />
+          <Route path={APP_ROUTES.BUS_EDIT} element={<EditBusPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,4 +1,6 @@
 export const APP_ROUTES = {
-    TASK: "/",
-    BUS_CRUD: "/buses",
-}
+  TASK: "/",
+  BUS_CRUD: "/buses",
+  BUS_DETAILS: "/buses/:id",
+  BUS_EDIT: "/buses/:id/edit",
+};
