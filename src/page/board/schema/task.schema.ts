280be74edded14d31f5cf299a327bb47f5a86997
Florian Lovasi <lovasifloriian@outlook.hu>
2026-05-30 15:55:20 +0200

    (refact): use proper naming in task schema

--- src/page/board/schema/task.schema.ts
+++ src/page/board/schema/task.schema.ts
@@ -1,9 +1,9 @@
 import z from "zod";
 import type { TaskStatus } from "../board.types";
 
-export const taskSchema = z.object({
+export const createOreditTaskSchemma = z.object({
   title: z.string().min(1, "Név megadása kötelező"),
   status: z.enum(["done", "in_progress", "todo"] satisfies TaskStatus[]),
 });
 
-export type CreateOrEditTaskInput = z.infer<typeof taskSchema>;
+export type CreateOrEditTaskInput = z.infer<typeof createOreditTaskSchemma>;
